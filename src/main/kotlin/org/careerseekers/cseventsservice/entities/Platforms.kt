194eab58aa14dd207a36b91c14af53acb5e074e1
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-26 17:50:18 +0300

    feat(db): created Platforms table

---
+++ src/main/kotlin/org/careerseekers/cseventsservice/entities/Platforms.kt
@@ -0,0 +1,32 @@
+package org.careerseekers.cseventsservice.entities
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "platforms")
+data class Platforms(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long,
+
+    @Column(nullable = false)
+    var fullName: String,
+
+    @Column(nullable = false)
+    var shortName: String,
+
+    @Column(nullable = false)
+    var address: String,
+
+    @Column(nullable = false)
+    var verified: Boolean = false,
+
+    @Column(nullable = false)
+    var userId: Long? = null,
+)
