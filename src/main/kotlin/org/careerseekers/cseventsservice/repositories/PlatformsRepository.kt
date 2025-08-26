93306b9b5034f23d151a83886b40fb4e9d3ebe5e
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-26 17:52:21 +0300

    feat(db): created platforms repository

---
+++ src/main/kotlin/org/careerseekers/cseventsservice/repositories/PlatformsRepository.kt
@@ -0,0 +1,8 @@
+package org.careerseekers.cseventsservice.repositories
+
+import org.careerseekers.cseventsservice.entities.Platforms
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface PlatformsRepository : JpaRepository<Platforms, Long>
