b0d46336d2739b16e255406c2cf6581cc1e0a974
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-27 01:36:16 +0300

    test(platforms): created tests for create and createAll functions

---
+++ src/test/kotlin/org/careerseekers/cseventsservice/services/platformsService/PlatformServiceCreateTests.kt
@@ -0,0 +1,77 @@
+package org.careerseekers.cseventsservice.services.platformsService
+
+import io.mockk.every
+import io.mockk.verify
+import org.assertj.core.api.Assertions.assertThat
+import org.careerseekers.cseventsservice.exceptions.NotFoundException
+import org.careerseekers.cseventsservice.mocks.PlatformServiceMocks
+import org.careerseekers.cseventsservice.mocks.generators.CachedUsersGenerator.createUser
+import org.careerseekers.cseventsservice.mocks.generators.PlatformGenerator.createPlatform
+import org.careerseekers.cseventsservice.mocks.generators.PlatformGenerator.createPlatformDto
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import kotlin.test.assertFailsWith
+
+class PlatformServiceCreateTests : PlatformServiceMocks() {
+
+    @Nested
+    inner class CreateTests {
+
+        @Test
+        fun `Should create and return platform`() {
+            val user = createUser()
+            val platform = createPlatform().copy(userId = user.id)
+            val platformDto = createPlatformDto(platform)
+
+            every { usersCacheClient.getItemFromCache(any()) } returns user
+            every { platformsMapper.platformFromDto(any()) } returns platform
+            every { repository.save(any()) } returns platform
+
+            val result = serviceUnderTest.create(platformDto)
+
+            assertThat(result).isNotNull.isEqualTo(platform)
+
+            verify { usersCacheClient.getItemFromCache(platformDto.userId) }
+            verify { platformsMapper.platformFromDto(any()) }
+            verify { repository.save(platform) }
+        }
+
+        @Test
+        fun `Should throw NotFoundException when user not found`() {
+            val userId = 1L
+            val platform = createPlatform().copy(userId = userId)
+            val platformDto = createPlatformDto(platform)
+
+            every { usersCacheClient.getItemFromCache(any()) } returns null
+
+            val exception = assertFailsWith<NotFoundException> {
+                serviceUnderTest.create(platformDto)
+            }
+
+            assertThat(exception.message).isEqualTo("User with id $userId not found.")
+
+            verify { usersCacheClient.getItemFromCache(platformDto.userId) }
+
+            verify(exactly = 0) { platformsMapper.platformFromDto(any()) }
+            verify(exactly = 0) { repository.save(platform) }
+        }
+    }
+
+    @Nested
+    inner class CreateAllTests {
+
+        @Test
+        fun `Should create all platforms`() {
+            val dummyPlatform = createPlatform()
+            val platforms = List(5) { createPlatformDto(dummyPlatform) }
+
+            every { serviceUnderTest.create(any()) } returns dummyPlatform
+
+            val result = serviceUnderTest.createAll(platforms)
+
+            assertThat(result).isNotNull.isEqualTo("Platforms created successfully.")
+
+            verify(exactly = 5) { serviceUnderTest.create(any())  }
+        }
+    }
+}
