6afa65b3a2f4bdbbf6cd0f888c44fc7a5035160b
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-26 12:10:25 +0300

    build(core): created basic serializers

---
+++ src/main/kotlin/org/careerseekers/cseventsservice/serializers/CustomSerializerModule.kt
@@ -0,0 +1,21 @@
+package org.careerseekers.cseventsservice.serializers
+
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.polymorphic
+import org.careerseekers.cseventsservice.dto.CachesDto
+import org.careerseekers.cseventsservice.dto.UsersCacheDto
+
+object CustomSerializerModule {
+    val customSerializerModule = SerializersModule {
+        polymorphic(CachesDto::class) {
+            subclass(UsersCacheDto::class, UsersCacheDto.serializer())
+        }
+    }
+
+    val json = Json {
+        serializersModule = customSerializerModule
+        classDiscriminator = "type"
+        ignoreUnknownKeys = true
+    }
+}
---
+++ src/main/kotlin/org/careerseekers/cseventsservice/serializers/PolymorphicRedisSerializer.kt
@@ -0,0 +1,27 @@
+package org.careerseekers.cseventsservice.serializers
+
+import kotlinx.serialization.KSerializer
+import org.careerseekers.cseventsservice.dto.CachesDto
+import org.springframework.data.redis.serializer.RedisSerializer
+import org.springframework.stereotype.Component
+import kotlin.collections.isEmpty
+import kotlin.collections.toString
+import kotlin.text.toByteArray
+
+@Suppress("UNCHECKED_CAST")
+@Component
+class PolymorphicRedisSerializer<Base : CachesDto> : RedisSerializer<Base> {
+
+    private val json = CustomSerializerModule.json
+    private val baseSerializer = CachesDto.serializer() as KSerializer<Base>
+
+    override fun serialize(t: Base?): ByteArray? {
+        if (t == null) return null
+        return json.encodeToString(baseSerializer, t).toByteArray(Charsets.UTF_8)
+    }
+
+    override fun deserialize(bytes: ByteArray?): Base? {
+        if (bytes == null || bytes.isEmpty()) return null
+        return json.decodeFromString(baseSerializer, bytes.toString(Charsets.UTF_8))
+    }
+}
