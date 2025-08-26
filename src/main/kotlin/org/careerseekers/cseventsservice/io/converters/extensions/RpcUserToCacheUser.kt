69bef803ae9bfbf4cd5640497329d68f5d0ac88f
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-26 14:56:06 +0300

    build(core): gRPC integration

---
+++ src/main/kotlin/org/careerseekers/cseventsservice/io/converters/extensions/RpcUserToCacheUser.kt
@@ -0,0 +1,22 @@
+package org.careerseekers.cseventsservice.io.converters.extensions
+
+import com.careerseekers.grpc.users.User
+import org.careerseekers.cseventsservice.dto.UsersCacheDto
+import org.careerseekers.cseventsservice.enums.UsersRoles
+
+fun User.toCache() : UsersCacheDto {
+    return UsersCacheDto(
+        id = this.id,
+        firstName = this.firstName,
+        lastName = this.lastName,
+        patronymic = this.patronymic,
+        dateOfBirth = this.dateOfBirth.toDate(),
+        email = this.email,
+        mobileNumber = this.mobileNumber,
+        password = this.password,
+        role = UsersRoles.valueOf(this.role),
+        avatarId = this.avatarId,
+        verified = this.verified,
+        isMentor = this.isMentor,
+    )
+}
