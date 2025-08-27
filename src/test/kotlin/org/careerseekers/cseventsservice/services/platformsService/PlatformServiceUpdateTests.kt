33739b277f454f008b66999ed2e81f4952f47544
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-08-27 03:01:12 +0300

    test(platforms): created tests for update platform owner function

--- src/test/kotlin/org/careerseekers/cseventsservice/services/platformsService/PlatformServiceUpdateTests.kt
+++ src/test/kotlin/org/careerseekers/cseventsservice/services/platformsService/PlatformServiceUpdateTests.kt
@@ -3,9 +3,11 @@ package org.careerseekers.cseventsservice.services.platformsService
 import io.mockk.every
 import io.mockk.verify
 import org.assertj.core.api.Assertions.assertThat
+import org.careerseekers.cseventsservice.dto.platforms.ChangePlatformOwnerDto
 import org.careerseekers.cseventsservice.dto.platforms.UpdatePlatformDto
 import org.careerseekers.cseventsservice.exceptions.NotFoundException
 import org.careerseekers.cseventsservice.mocks.PlatformServiceMocks
+import org.careerseekers.cseventsservice.mocks.generators.CachedUsersGenerator.createUser
 import org.careerseekers.cseventsservice.mocks.generators.PlatformGenerator.createPlatform
 import org.junit.jupiter.api.Nested
 import kotlin.test.Test
@@ -70,6 +72,69 @@ class PlatformServiceUpdateTests : PlatformServiceMocks() {
             assertThat(platform.address).isEqualTo("Address")
 
             verify { serviceUnderTest.getById(any(), any(), any()) }
+            verify(exactly = 0) { repository.save(any()) }
+        }
+    }
+
+    @Nested
+    inner class UpdatePlatformOwnerTests {
+
+        private val user = createUser()
+        private val platform = createPlatform().copy(userId = 1L)
+        private val dto = ChangePlatformOwnerDto(
+            id = platform.id,
+            userId = user.id,
+        )
+
+        @Test
+        fun `Should update platform owner and return String`() {
+            every { serviceUnderTest.getById(any(), any(), any()) } returns platform
+            every { usersCacheClient.getItemFromCache(any()) } returns user
+            every { repository.save(any()) } returns platform
+
+            val result = serviceUnderTest.updatePlatformOwner(dto)
+
+            assertThat(result).isNotNull.isEqualTo("Platform owner updated successfully.")
+
+            verify { serviceUnderTest.getById(any(), any(), any()) }
+            verify { usersCacheClient.getItemFromCache(any()) }
+            verify { repository.save(any()) }
+        }
+
+        @Test
+        fun `Should return NotFoundException when platform is null`() {
+            every {
+                serviceUnderTest.getById(
+                    any(),
+                    any(),
+                    any()
+                )
+            } throws NotFoundException("Platform with id ${platform.id} not found")
+
+            val exception = assertFailsWith<NotFoundException> {
+                serviceUnderTest.updatePlatformOwner(dto)
+            }
+
+            assertThat(exception.message).isEqualTo("Platform with id ${platform.id} not found")
+
+            verify { serviceUnderTest.getById(any(), any(), any()) }
+            verify(exactly = 0) { usersCacheClient.getItemFromCache(any()) }
+            verify(exactly = 0) { repository.save(any()) }
+        }
+
+        @Test
+        fun `Should throw NotFoundException when user not found in database`() {
+            every { serviceUnderTest.getById(any(), any(), any()) } returns platform
+            every { usersCacheClient.getItemFromCache(any()) } throws NotFoundException("User with id ${user.id} not found.")
+
+            val exception = assertFailsWith<NotFoundException> {
+                serviceUnderTest.updatePlatformOwner(dto)
+            }
+
+            assertThat(exception.message).isEqualTo("User with id ${user.id} not found.")
+
+            verify { serviceUnderTest.getById(any(), any(), any()) }
+            verify { usersCacheClient.getItemFromCache(any()) }
             verify(exactly = 0) { repository.save(any()) }
         }
     }
