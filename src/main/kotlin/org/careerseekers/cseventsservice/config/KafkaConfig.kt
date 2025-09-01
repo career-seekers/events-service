daad2096c575c71fac661cd5a1bb6c3e8e074aaf
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-09-02 00:40:40 +0300

    feat(kafka): created kafka topic

---
+++ src/main/kotlin/org/careerseekers/cseventsservice/config/KafkaConfig.kt
@@ -0,0 +1,20 @@
+package org.careerseekers.cseventsservice.config
+
+import org.apache.kafka.clients.admin.NewTopic
+import org.careerseekers.cseventsservice.enums.KafkaTopics
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.TopicBuilder
+
+@Configuration
+class KafkaConfig {
+
+    @Bean
+    fun platformCreationTopic(): NewTopic {
+        return TopicBuilder
+            .name(KafkaTopics.PLATFORM_CREATION.name)
+            .partitions(12)
+            .replicas(3)
+            .build()
+    }
+}
