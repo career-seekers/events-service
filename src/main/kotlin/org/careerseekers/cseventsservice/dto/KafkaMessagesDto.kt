2fbb3041a8ca62c20b102a328a823540fb1c4a43
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-09-02 00:45:05 +0300

    feat(kafka): created platform creation serializable dto

---
+++ src/main/kotlin/org/careerseekers/cseventsservice/dto/KafkaMessagesDto.kt
@@ -0,0 +1,24 @@
+package org.careerseekers.cseventsservice.dto
+
+import kotlinx.serialization.Polymorphic
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+@Polymorphic
+sealed class KafkaMessagesDto : DtoClass
+
+@Serializable
+data class PlatformDto(
+    val id: Long,
+    val fullName: String,
+    val shortName: String,
+    val address: String,
+    val userId: Long?,
+)
+
+@Serializable
+@SerialName("PlatformCreation")
+data class PlatformCreation (
+    val platform: PlatformDto
+) : KafkaMessagesDto()
--- src/main/kotlin/org/careerseekers/cseventsservice/serializers/CustomSerializerModule.kt
+++ src/main/kotlin/org/careerseekers/cseventsservice/serializers/CustomSerializerModule.kt
@@ -4,12 +4,17 @@ import kotlinx.serialization.json.Json
 import kotlinx.serialization.modules.SerializersModule
 import kotlinx.serialization.modules.polymorphic
 import org.careerseekers.cseventsservice.dto.CachesDto
+import org.careerseekers.cseventsservice.dto.KafkaMessagesDto
+import org.careerseekers.cseventsservice.dto.PlatformCreation
 import org.careerseekers.cseventsservice.dto.UsersCacheDto
 
 object CustomSerializerModule {
     val customSerializerModule = SerializersModule {
         polymorphic(CachesDto::class) {
             subclass(UsersCacheDto::class, UsersCacheDto.serializer())
+        }
+        polymorphic(KafkaMessagesDto::class) {
+            subclass(PlatformCreation::class, PlatformCreation.serializer())
         }
     }
 
