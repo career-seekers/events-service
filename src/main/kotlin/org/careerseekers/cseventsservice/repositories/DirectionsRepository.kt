093bc56df4d6bb9c18d2152228ab386035037375
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-09-09 02:43:40 +0300

    feat(db): created directions repository

---
+++ src/main/kotlin/org/careerseekers/cseventsservice/repositories/DirectionsRepository.kt
@@ -0,0 +1,12 @@
+package org.careerseekers.cseventsservice.repositories
+
+import org.careerseekers.cseventsservice.entities.Directions
+import org.careerseekers.cseventsservice.enums.DirectionAgeCategory
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface DirectionsRepository : JpaRepository<Directions, Long> {
+    fun getByUserId(userId: Long): List<Directions>
+    fun getByAgeCategory(ageCategory: DirectionAgeCategory): List<Directions>
+}
