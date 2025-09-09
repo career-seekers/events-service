48579f6a74817dd3c5e3dd03ff613f9310c07d29
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-09-09 03:45:53 +0300

    feat(api): created directions controller

---
+++ src/main/kotlin/org/careerseekers/cseventsservice/controllers/DirectionController.kt
@@ -0,0 +1,59 @@
+package org.careerseekers.cseventsservice.controllers
+
+import org.careerseekers.cseventsservice.controllers.interfaces.CrudController
+import org.careerseekers.cseventsservice.dto.directions.CreateDirectionDto
+import org.careerseekers.cseventsservice.dto.directions.UpdateDirectionDto
+import org.careerseekers.cseventsservice.entities.Directions
+import org.careerseekers.cseventsservice.enums.DirectionAgeCategory
+import org.careerseekers.cseventsservice.io.converters.extensions.toHttpResponse
+import org.careerseekers.cseventsservice.services.DirectionsService
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/events-service/v1/directions")
+class DirectionController(
+    override val service: DirectionsService,
+) : CrudController<Directions, Long, CreateDirectionDto, UpdateDirectionDto> {
+
+    @GetMapping("/")
+    override fun getAll() = service.getAll().toHttpResponse()
+
+    @GetMapping("/{id}")
+    override fun getById(@PathVariable id: Long) =
+        service.getById(id, message = "Direction with id $id not found.")!!.toHttpResponse()
+
+    @GetMapping("/getByUserId/{userId}")
+    fun getByUserId(@PathVariable userId: Long) = service.getByUserId(userId).toHttpResponse()
+
+    @GetMapping("/getByAgeCategory/{ageCategory}")
+    fun getByAgeCAtegory(@PathVariable ageCategory: DirectionAgeCategory) =
+        service.getByAgeCategory(ageCategory).toHttpResponse()
+
+    @GetMapping("/getByAge/{age}")
+    fun getByAge(@PathVariable age: Short) =
+        service.getByAgeCategory(DirectionAgeCategory.getAgeCategory(age)).toHttpResponse()
+
+    @PostMapping("/")
+    override fun create(@RequestBody item: CreateDirectionDto) = service.create(item).toHttpResponse()
+
+    @PostMapping("/createAll")
+    override fun createAll(items: List<CreateDirectionDto>) = service.createAll(items).toHttpResponse()
+
+    @PatchMapping("/")
+    override fun update(@RequestBody item: UpdateDirectionDto) = service.update(item).toHttpResponse()
+
+    @DeleteMapping("/{id}")
+    override fun deleteById(@PathVariable id: Long) = service.deleteById(id).toHttpResponse()
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/")
+    override fun deleteAll() = service.deleteAll().toHttpResponse()
+}
