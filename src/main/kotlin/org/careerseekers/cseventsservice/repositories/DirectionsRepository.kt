f1b39ab836e10c0b60aa5ae84e0d82b2159cb12b
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-09-15 00:55:10 +0300

    fix(repository): fixed getByAgeCategory function

--- src/main/kotlin/org/careerseekers/cseventsservice/repositories/DirectionsRepository.kt
+++ src/main/kotlin/org/careerseekers/cseventsservice/repositories/DirectionsRepository.kt
@@ -3,11 +3,15 @@ package org.careerseekers.cseventsservice.repositories
 import org.careerseekers.cseventsservice.entities.Directions
 import org.careerseekers.cseventsservice.enums.DirectionAgeCategory
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
 interface DirectionsRepository : JpaRepository<Directions, Long> {
     fun findByUserId(userId: Long): List<Directions>
     fun findByExpertId(expertId: Long): List<Directions>
-    fun findByAgeCategory(ageCategory: DirectionAgeCategory): List<Directions>
+
+    @Query("select d from Directions d join d.ageCategories a where a.ageCategory = :ageCategory")
+    fun findByAgeCategory(@Param("ageCategory") ageCategory: DirectionAgeCategory): List<Directions>
 }
