56de6e263611aab8d84251a40f14b23fcc8bd8f8
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-10-15 03:27:26 +0300

    refactor(aop): created basic interface for aop services

--- src/main/kotlin/org/careerseekers/cseventsservice/aspects/PlatformUpdatesAspect.kt
+++ src/main/kotlin/org/careerseekers/cseventsservice/aspects/PlatformUpdatesAspect.kt
@@ -3,6 +3,7 @@ package org.careerseekers.cseventsservice.aspects
 import org.aspectj.lang.JoinPoint
 import org.aspectj.lang.annotation.AfterReturning
 import org.aspectj.lang.annotation.Aspect
+import org.careerseekers.cseventsservice.aspects.interfaces.IEntityUpdatesAspect
 import org.careerseekers.cseventsservice.utils.StatisticsScrapperService
 import org.careerseekers.cseventsservice.utils.StatisticsStorage
 import org.slf4j.LoggerFactory
@@ -10,12 +11,12 @@ import org.springframework.stereotype.Component
 
 @Aspect
 @Component
-class PlatformUpdatesAspect(private val statisticsScrapperService: StatisticsScrapperService) {
+class PlatformUpdatesAspect(private val statisticsScrapperService: StatisticsScrapperService) : IEntityUpdatesAspect {
 
     private val logger = LoggerFactory.getLogger(this.javaClass)
 
     @AfterReturning("@annotation(org.careerseekers.cseventsservice.annotations.PlatformsUpdate)")
-    fun afterPlatformChange(joinPoint: JoinPoint) {
+    override fun afterUpdate(joinPoint: JoinPoint) {
         statisticsScrapperService.setPlatformsCount()
 
         logger.info("Platforms count updated, the caller method is ${joinPoint.signature.name}. Total platforms: ${StatisticsStorage.platformsCount}.")
