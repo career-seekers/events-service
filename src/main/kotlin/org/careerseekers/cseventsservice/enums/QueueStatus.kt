f044220d85008a0f696670fbaa519a296da44b1a
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-10-17 07:59:05 +0300

    feat(enums): created alias for queue statuses

--- src/main/kotlin/org/careerseekers/cseventsservice/enums/QueueStatus.kt
+++ src/main/kotlin/org/careerseekers/cseventsservice/enums/QueueStatus.kt
@@ -1,6 +1,10 @@
 package org.careerseekers.cseventsservice.enums
 
-enum class QueueStatus {
-    PARTICIPATES,
-    IN_QUEUE,
+enum class QueueStatus(private val alias: String) {
+    PARTICIPATES("Участвует"),
+    IN_QUEUE("В очереди");
+
+    companion object {
+        fun QueueStatus.getAlias() = this.alias
+    }
 }
