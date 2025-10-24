b379daab9cd34e1f32ff7ef03346c9c2c0afecd2
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-10-24 10:22:14 +0300

    fix(enums): changed age category alias due to a complaint from the most positive teacher

--- src/main/kotlin/org/careerseekers/cseventsservice/enums/DirectionAgeCategory.kt
+++ src/main/kotlin/org/careerseekers/cseventsservice/enums/DirectionAgeCategory.kt
@@ -5,7 +5,7 @@ import org.careerseekers.cseventsservice.exceptions.BadRequestException
 enum class DirectionAgeCategory(private val ageAlias: String) {
     PRESCHOOL_1("4-5 лет"),
     PRESCHOOL_2("6-7 лет"),
-    SCHOOL_1("7-9 лет"),
+    SCHOOL_1("7-8 лет"),
     SCHOOL_2("9-11 лет"),
     SCHOOL_3("12-13 лет");
 
