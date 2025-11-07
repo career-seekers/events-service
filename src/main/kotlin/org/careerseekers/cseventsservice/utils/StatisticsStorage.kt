c709ab4e846916d0963739b46012cea0e1f1e00d
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-11-07 18:32:29 +0300

    refactor(statistics): rewrote data types from long to AtomicLong

--- src/main/kotlin/org/careerseekers/cseventsservice/utils/StatisticsStorage.kt
+++ src/main/kotlin/org/careerseekers/cseventsservice/utils/StatisticsStorage.kt
@@ -1,32 +1,33 @@
 package org.careerseekers.cseventsservice.utils
 
 import java.time.LocalDateTime
+import java.util.concurrent.atomic.AtomicLong
 
 object StatisticsStorage {
-    var platformsCount: Long = 0
+    var platformsCount: AtomicLong = AtomicLong(0)
         private set
 
-    var verifiedPlatformsCount: Long = 0
+    var verifiedPlatformsCount: AtomicLong = AtomicLong(0)
         private set
 
-    var directionsCount: Long = 0
+    var directionsCount: AtomicLong = AtomicLong(0)
         private set
 
-    var directionsWithoutDocs: Long = 0
+    var directionsWithoutDocs: AtomicLong = AtomicLong(0)
         private set
 
-    var directionDocsCount: Long = 0
+    var directionDocsCount: AtomicLong = AtomicLong(0)
         private set
 
     var lastDocumentUpload: LocalDateTime? = null
         private set
 
-    fun setPlatformsCount(platformsCount: Long) = apply { this.platformsCount = platformsCount }
-    fun setVerifiedPlatformsCount(platformsCount: Long) = apply { this.verifiedPlatformsCount = platformsCount }
+    fun setPlatformsCount(platformsCount: Long) = apply { this.platformsCount.set(platformsCount) }
+    fun setVerifiedPlatformsCount(platformsCount: Long) = apply { this.verifiedPlatformsCount.set(platformsCount) }
 
-    fun setDirectionsCount(count: Long) = apply { this.directionsCount = count }
-    fun setDirectionsWithoutDocs(count: Long) = apply { this.directionsWithoutDocs = count }
+    fun setDirectionsCount(count: Long) = apply { this.directionsCount.set(count) }
+    fun setDirectionsWithoutDocs(count: Long) = apply { this.directionsWithoutDocs.set(count) }
 
-    fun setDirectionDocsCount(count: Long) = apply { this.directionDocsCount = count }
+    fun setDirectionDocsCount(count: Long) = apply { this.directionDocsCount.set(count) }
     fun setLastDocumentUpload(lastDocumentUpload: LocalDateTime?) = apply { this.lastDocumentUpload = lastDocumentUpload }
 }
