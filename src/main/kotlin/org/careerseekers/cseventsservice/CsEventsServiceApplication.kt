b0469c17b9abf4d8691dffd19fb5ab7a98ae0f5e
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2025-11-20 11:48:57 +0300

    feat(core): enable websocket brokers & create websocket config

--- src/main/kotlin/org/careerseekers/cseventsservice/CsEventsServiceApplication.kt
+++ src/main/kotlin/org/careerseekers/cseventsservice/CsEventsServiceApplication.kt
@@ -3,8 +3,10 @@ package org.careerseekers.cseventsservice
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.retry.annotation.EnableRetry
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 
 @EnableRetry
+@EnableWebSocketMessageBroker
 @SpringBootApplication
 class CsEventsServiceApplication
 
---
+++ src/main/kotlin/org/careerseekers/cseventsservice/config/WebSocketConfig.kt
@@ -0,0 +1,22 @@
+package org.careerseekers.cseventsservice.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+
+@Configuration
+class WebSocketConfig : WebSocketMessageBrokerConfigurer {
+
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        registry
+            .addEndpoint("/ws")
+            .setAllowedOrigins("*")
+            .withSockJS()
+    }
+
+    override fun configureMessageBroker(registry: MessageBrokerRegistry) {
+        registry.enableSimpleBroker("/topic")
+        registry.setApplicationDestinationPrefixes("/app")
+    }
+}
