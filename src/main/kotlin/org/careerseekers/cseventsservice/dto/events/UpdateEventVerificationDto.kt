6d0da132b078843458d7ef17662b12f41f0fb571
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2026-01-11 17:00:13 +0300

    refactor(events): updated events services & dtos for new verification types

--- src/main/kotlin/org/careerseekers/cseventsservice/dto/events/UpdateEventVerificationDto.kt
+++ src/main/kotlin/org/careerseekers/cseventsservice/dto/events/UpdateEventVerificationDto.kt
@@ -1,8 +1,9 @@
 package org.careerseekers.cseventsservice.dto.events
 
 import org.careerseekers.cseventsservice.dto.DtoClass
+import org.careerseekers.cseventsservice.enums.VerificationStatus
 
 data class UpdateEventVerificationDto(
     val id: Long,
-    val verified: Boolean,
+    val verified: VerificationStatus,
 ) : DtoClass
