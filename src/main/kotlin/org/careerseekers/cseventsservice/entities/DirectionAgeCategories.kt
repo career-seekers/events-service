42df2f76918e68112defdebb19a9947094b1d097
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2026-01-21 11:03:12 +0300

    feat(events): added events count to age categories

--- src/main/kotlin/org/careerseekers/cseventsservice/entities/DirectionAgeCategories.kt
+++ src/main/kotlin/org/careerseekers/cseventsservice/entities/DirectionAgeCategories.kt
@@ -2,19 +2,10 @@ package org.careerseekers.cseventsservice.entities
 
 import com.fasterxml.jackson.annotation.JsonIgnore
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import jakarta.persistence.CascadeType
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.FetchType
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.ManyToOne
-import jakarta.persistence.OneToMany
-import jakarta.persistence.Table
+import jakarta.persistence.*
 import jakarta.validation.constraints.Min
 import org.careerseekers.cseventsservice.enums.DirectionAgeCategory
+import kotlin.jvm.Transient
 
 @Entity
 @Table(name = "direction_age_categories")
@@ -50,4 +41,7 @@ data class DirectionAgeCategories(
 ) {
     val currentParticipantsCount: Long
         get() = participants?.size?.toLong() ?: 0L
+
+    val eventsCount: Int
+        get() = events?.size ?: 0
 }
