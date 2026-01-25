ac216a19f0aaa20cd638d71874ce8ed35d1c0a64
Vladimir Fokin <115186975+scobca@users.noreply.github.com>
2026-01-25 03:14:07 +0300

    feat(statistics): updated WebSocket statistics controller

--- src/main/kotlin/org/careerseekers/cseventsservice/controllers/WebSocketStatisticController.kt
+++ src/main/kotlin/org/careerseekers/cseventsservice/controllers/WebSocketStatisticController.kt
@@ -6,6 +6,7 @@ import org.springframework.messaging.handler.annotation.SendTo
 import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.stereotype.Controller
 import java.time.LocalDateTime
+import java.util.concurrent.atomic.AtomicInteger
 import java.util.concurrent.atomic.AtomicLong
 
 @Controller
@@ -18,6 +19,8 @@ class WebSocketStatisticController(private val messagingTemplate: SimpMessagingT
         val directionsWithoutDocs: AtomicLong,
         val directionDocsCount: AtomicLong,
         val lastDocumentUpload: LocalDateTime?,
+        val eventsCount: AtomicInteger,
+        val verifiedEventsCount: AtomicInteger,
     )
 
     @MessageMapping("/getStatistics")
@@ -29,7 +32,9 @@ class WebSocketStatisticController(private val messagingTemplate: SimpMessagingT
             directionsCount = StatisticsStorage.directionsCount,
             directionsWithoutDocs = StatisticsStorage.directionsWithoutDocs,
             directionDocsCount = StatisticsStorage.directionDocsCount,
-            lastDocumentUpload = StatisticsStorage.lastDocumentUpload
+            lastDocumentUpload = StatisticsStorage.lastDocumentUpload,
+            eventsCount = StatisticsStorage.eventsCount,
+            verifiedEventsCount = StatisticsStorage.verifiedEventsCount,
         )
     }
 
